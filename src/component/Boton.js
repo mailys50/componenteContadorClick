cb432f8fd0329879f8e6fa3cb17cedbb91bc35e4
Mailys <77859157+mailys50@users.noreply.github.com>
2023-03-20 19:32:42 -0300

    creación de estructurra e inicio de funcionalidad del boton

---
+++ src/component/Boton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../estilos/BotonEstilo.css';
+function Boton({ texto, esBotonClic,manejarClic }) {
+    return(
+        <button
+        className={esBotonClic ? 'botonClic' : 'botonReset'}
+        onClick={manejarClic}>
+        {texto} 
+        </button>
+    );
+}
+
+export default Boton;
+
